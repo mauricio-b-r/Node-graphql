fa14d4b5a7f1a40bec5337fe9caf2f05ffac2d77
unknown <mauricioblancoregojo@gmail.com>
2021-09-27 15:01:00 -0300

    added mongo db and mongoose, added env vars to nodemon

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const { graphqlHTTP } = require('express-graphql')
-const { buildSchema } = require("graphql")
+const express = require("express");
+const bodyParser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+const Event = require("./models/event")
 
-const app = express()
 
-const events = []
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // app.get("/", (req,res,next) => {
 //     res.send("Hello World")
 // })
 
-app.use("/graphql", graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema: buildSchema(`
         type Event {
             _id: ID!
@@ -42,21 +45,43 @@ app.use("/graphql", graphqlHTTP({
         }
     `),
     rootValue: {
-        events: () => {
-            return events
-        },
-        createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: args.eventInput.date,
-            }
-            events.push(event)
-            return event
-        }
+      events: () => {
+        return Event.find().then(events => {
+            return events.map(event => {
+                return {...event._doc, _id: event.id}
+            })
+        }).catch(err => {
+            throw err
+        });
+      },
+      createEvent: (args) => {
+        const event = new Event({
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date: new Date(args.eventInput.date),
+        })
+        return event.save().then(result => {
+            console.log(result)
+            // return {...result._doc, _id: event.id}
+            return {...result._doc, _id: result._doc._id.toString()}
+        }).catch(err => {
+            console.log(err)
+            throw err
+        })
+      },
     },
-    graphiql: true
-}))
-app.listen(3000);
+    graphiql: true,
+  })
+);
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster-node-course.zmjl3.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+      app.listen(3000)
+  })
+  .catch((err) => {
+    console.log(err);
+  });
