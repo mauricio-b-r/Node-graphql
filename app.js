6fd47f45bc46c163bef52c4df2fc56aae8ed5d9e
unknown <mauricioblancoregojo@gmail.com>
2021-09-27 17:43:21 -0300

    booking model and code refactoring

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
 const mongoose = require("mongoose");
 
-const graphQlSchema = require("./graphql/schema/index")
-const graphQlResolvers = require("./graphql/resolvers/index")
+const graphQlSchema = require("./graphql/schema/index");
+const graphQlResolvers = require("./graphql/resolvers/index");
 
 const app = express();
 
@@ -28,7 +28,7 @@ mongoose
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster-node-course.zmjl3.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
   )
   .then(() => {
-      app.listen(3000)
+    app.listen(3000);
   })
   .catch((err) => {
     console.log(err);
